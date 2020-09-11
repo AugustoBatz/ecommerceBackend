d267f1d8b97b4268673c9b3572d683b54a9c8eaf
Augusto Batz <miguelbatz2@gmail.com>
2020-09-10 18:37:31 -0600

    add new module User

---
+++ user/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from user.models import User
+from user.serializers import UserSerializer
+# Create your views here.
+
+
+@api_view(['GET', 'POST'])
+def user_list(request):
+    """
+    List all code Books, or create a new Book.
+    """
+    print('insade user list')
+    if request.method == 'GET':
+        print('insade get')
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        print('insade post')
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
