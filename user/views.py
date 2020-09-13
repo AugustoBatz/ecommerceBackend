720629b7872d0bcb7d6fa59bb73d5ad6c0aa5911
Augusto Batz <miguelbatz2@gmail.com>
2020-09-13 15:40:57 -0600

    Add mocks

--- user/views.py
+++ user/views.py
@@ -14,12 +14,20 @@ def user_list(request):
     """
     print('insade user list')
     if request.method == 'GET':
+        is_mock = request.headers['Mock']
+        if (is_mock == 'True'):
+            return Response(list, status=status.HTTP_200_OK)
         print('insade get')
         users = User.objects.all()
         serializer = UserSerializer(users, many=True)
         return Response(serializer.data)
 
     elif request.method == 'POST':
+        print(request.headers)
+        is_mock = request.headers['Mock']
+        print('value of mock ' + is_mock)
+        if (is_mock == 'True'):
+           return Response(request.data, status=status.HTTP_201_CREATED) 
         print('insade post')
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
