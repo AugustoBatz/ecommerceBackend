c503c694d37d3a08b2398db05f5847ff17a367f8
Augusto Batz <miguelbatz2@gmail.com>
2020-09-13 20:49:29 -0600

    App for Products & stock

---
+++ product/models.py
@@ -0,0 +1,27 @@
+from django.db import ConnectionHandler, models
+
+# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=45)
+    code = models.CharField(max_length=10, unique=True)
+    category = models.CharField(max_length=25)
+    brand = models.CharField(max_length=25)
+
+class Color(models.Model):
+    color = models.CharField(max_length=15)
+    
+class Size(models.Model):
+    size = models.CharField(max_length=15)
+
+class ProductDetail(models.Model):
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+    size_id = models.ForeignKey(Size, on_delete=models.CASCADE)
+    color_id = models.ForeignKey(Color, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    price = models.FloatField()
+
+class PurchaseDetail(models.Model):
+    date = models.DateTimeField(auto_now_add=True)
+    quantity = models.IntegerField()
+    detail_product = models.ForeignKey(ProductDetail, on_delete=models.CASCADE)
