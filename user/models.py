8eecb929f0a9fb842b74a47848261f8ab4bd185b
Augusto Batz <miguelbatz2@gmail.com>
2020-09-13 20:49:54 -0600

    Complete field for user

--- user/models.py
+++ user/models.py
@@ -2,6 +2,11 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    first_name = models.CharField(max_length=30)
+    name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     phone = models.IntegerField()
+    email = models.EmailField()
+    is_admin = models.BooleanField(default=False)
+    address_a = models.CharField(max_length=45)
+    address_b = models.CharField(max_length=45)
+    password = models.CharField(max_length=45)
