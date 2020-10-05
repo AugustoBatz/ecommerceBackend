39adf911ec51355569011c39379dc1d0c2f3fab2
kheysser <kheysser@outlook.com>
2020-10-05 16:43:50 -0600

    arreglo CRUD productos

--- product/views.py
+++ product/views.py
@@ -18,7 +18,7 @@ class productoAPIView(APIView):
         serializer = ProductoSerializers(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTTP_201_CREATED)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
