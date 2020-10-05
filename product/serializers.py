d27da5ff957b961c0755455a9669c519fcc08dd0
Augusto Batz <miguelbatz2@gmail.com>
2020-10-04 22:41:24 -0600

    Format code

--- product/serializers.py
+++ product/serializers.py
@@ -1,10 +1,12 @@
 from rest_framework import serializers
 from .models import Product
 
+
 class ProductoSerializers(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = '__all__'
+
 
 class ProductoModificacionSerializers(serializers.ModelSerializer):
     class Meta:
