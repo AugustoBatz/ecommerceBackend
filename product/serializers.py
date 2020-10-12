da677d27080cf407934a5b1a089b528883a2fb84
ChrisQ97 <caqp14@gmail.com>
2020-10-12 02:09:48 -0600

    cambios imagen en producto

--- product/serializers.py
+++ product/serializers.py
@@ -11,11 +11,12 @@ class ProductoSerializers(serializers.ModelSerializer):
 class ProductoModificacionSerializers(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ['name', 'category', 'brand']
+        fields = ['name', 'category', 'brand', 'image']
     
     def update(self, instance, validated_data):
         instance.name = validated_data.get('name', instance.nombre)
         instance.category = validated_data.get('category', instance.category)
         instance.brand = validated_data.get('brand', instance.brand)
+        instance.image = validated_data.get('image', instance.image)
         instance.save()
         return instance
